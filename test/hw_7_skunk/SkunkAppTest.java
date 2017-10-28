7f494157efeadd8ec54e5d98b57d7c9b394240fe
Kevin Um <um001069@stthomas.edu>
2017-10-28 12:07:18 -0500

    Adds Dice class and Dice Class tests.

--- test/hw_7_skunk/SkunkAppTest.java
+++ test/hw_7_skunk/SkunkAppTest.java
@@ -12,8 +12,29 @@ public class SkunkAppTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void new_dice_can_be_created() {
+		Dice d = new Dice();
+		assertTrue("New Dice Instance not Null!", d != null);
+	}
+	
+	@Test
+	public void new_dice_rolls_double_skunk() {
+		Dice d = new Dice(new int[] {1}, new int[] {1});
+		d.roll();
+		assertEquals("First Die is not 1.", 1, d.getDie1Roll());
+		assertEquals("Second Die is not 1.", 1, d.getDie2Roll());
+	}
+	
+	@Test
+	public void new_dice_rolls_boxcars_then_double_skunk() {
+		Dice d = new Dice(new int[] {6,1}, new int[] {6, 1});
+		d.roll();
+		assertEquals("First Die is not 6.", 6, d.getDie1Roll());
+		assertEquals("Second Die is not 6.", 6, d.getDie2Roll());
+		
+		d.roll();
+		assertEquals("First Die is not 1.", 1, d.getDie1Roll());
+		assertEquals("Second Die is not 1.", 1, d.getDie2Roll());
 	}
 
 }
